7a54d6489d0c6252a2f908c786a9d6930edf6653
Shubham Khaire <shubhamkhaire523@gmail.com>
2026-05-14 08:04:26 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-  },
   plugins: [react()],
   base: "/portfolio/",
 });
